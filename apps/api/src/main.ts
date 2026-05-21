9865f9cdbbd2f86ff34e469f3fb05eadac5e45d4
James Booth <jpbooth@lattix.io>
2026-05-21 11:54:17 -0400

    fix(api): resolve runtime aliases on vercel

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import 'tsconfig-paths/register';
 import './config/load-env';
 import type { INestApplication } from '@nestjs/common';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
